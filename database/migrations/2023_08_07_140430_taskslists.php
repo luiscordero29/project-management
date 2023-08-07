e36192c289a313454ef6b7f471ff987981cf9a25
Luis Cordero <info@luiscordero29.com>
2023-08-07 10:14:56 -0400

    taskslists

---
+++ database/migrations/2023_08_07_140430_taskslists.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taskslists', function (Blueprint $table) {
+            $table->uuid('taskslist_id')->primary();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->uuid('milestone_id');
+            $table->foreign('milestone_id')->references('milestone_id')->on('milestones')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('taskslists', function (Blueprint $table) {
+            $table->dropForeign(['milestone_id']);
+        });
+        Schema::dropIfExists('taskslists');
+    }
+};
